8823200a28f6f069bdd03c2328a10b304a8f4ac1
wpnl <532118819@qq.com>
2018-01-09 16:38:29 +0800

    2018-1-9 16:38:21 结衣无敌提交

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,13 +17,21 @@
           <el-col :xs="24" :sm="8" :md="6" :lg="4" :xl="4" v-for="(item, index) in posts" :key="index">
             <div class="grid-content bg-purple">
               <div class="content-bg">
-                <img src="../assets/images/11.jpg" width="100%" height="100%">
+                <img src="../assets/images/11.jpg">
+                <span>博客名称</span>
               </div>
               <div class="content-text">
-                博客名称：{{ item.name }}
+                专业的博客平台
               </div>
-              <div class="content-text">
-                博客简介：{{ item.description }}
+              <div class="content-footer">
+                <div class="left-footer">
+                  <i class="el-icon-view"></i>
+                  <span>123</span>
+                </div>
+                <div class="right-footer">
+                  <i class="el-icon-star-on"></i>
+                  <span>321</span>
+                </div>
               </div>
             </div>
           </el-col>
@@ -159,25 +167,37 @@ export default {
   box-sizing: border-box;
   padding: 0 2.5%;
   .bg-purple {
-    background: #e2e2e2;
+    padding: 0 3%;
+    background: #fff;
     .content-bg {
+      margin: 5%;
       width: 100%;
-      height: 80px;
       img {
-        width: 100%;
-        height: 100%;
+        width: 30px;
+        height: 30px;
+      }
+      span {
+        margin-left: 8px;
+        font-size: 12px;
+        font-weight: bold;
       }
     }
     .content-text {
-      margin-top: 6px;
+      margin: 5%;      
       text-align: left;
       font-size: 12px;
+    }
+    .content-footer {
+      display: flex;
+      justify-content: space-between;
+      margin: 5% 5% 0;
+      font-size: 10px;
     }
   }
   .grid-content {
     margin-bottom: 5%;
     border-radius: 10px;
-    min-height: 120px;
+    min-height: 100px;
     padding: 6px;
     transition: 300ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
     &:hover {
