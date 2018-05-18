d4b8a02075c1d625d91e52a4e9416e9170db3245
ishidai <prosperdai5@gmail.com>
2018-05-18 23:33:45 +0800

    add footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,37 @@
 <template>
   <div class="footer">
-    <h3 class="item">博客咖-博客聚合导航站</h3>
-    <div class="footer-menu item">
-      <a style="" href="https://toit.me">联系我们</a>
-      <a style="" href="https://toit.me">关于我们</a>
-      <a style="" href="https://toit.me">反馈建议</a>
+    <div class="container line">
+        <div class="footer-navigation">
+            <dl>
+                <dt>关于</dt>
+                <dd><a href="https://www.bokeka.com">联系我们</a></dd>
+            </dl>
+            <dl>
+                <dt>帮助</dt>
+                <dd><a href="http://wpa.qq.com/msgrd?v=3&uin=478724&site=qq&menu=yes" target="_blank">提交bug</a></dd>
+            </dl>
+            <dl>
+                <dt>合作</dt>
+                <dd><a href="http://wpa.qq.com/msgrd?v=3&uin=478724&site=qq&menu=yes" target="_blank">商务合作</a></dd>
+                <dd><a href="http://wpa.qq.com/msgrd?v=3&uin=478724&site=qq&menu=yes" target="_blank">友链互换</a></dd>
+            </dl>
+            <dl class="width-fix">
+                <dt>友链</dt>
+                <dd><a href="https://bokeka.com" target="_blank" rel="nofollow">找呀找朋友</a></dd>
+            </dl>
+            <dl class="no-dt">
+                <dd><a href="/" target="_blank">虚席以待</a></dd>
+			      </dl>
+            <dl class="no-dt">
+                <dd><a href="/" target="_blank" rel="nofollow">资源大爆炸</a></dd>
+                <dd><a href="/" target="_blank" rel="nofollow">在线工具</a></dd>
+            </dl>
+        </div>
     </div>
-    <p>&copy; &nbsp;2018 &nbsp;Bokeka.com&nbsp;版权所有</p>
+    <div class="container">
+        <a class="copyright">Copyright &copy; 2017-2018 bokeka.com. All rights reserved.</a>
+    </div>
+
   </div>
 </template>
 
@@ -19,25 +44,106 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  .footer{
-    background-color: rgba(0,101,206,0.9);
-    padding: 2rem;
-    font-size: 1.3rem;
-    margin-top: 1.0rem;
-    h3{
-      font-size: 1.8rem;
-    }
-    .footer-menu{
-      display: flex;
-      justify-content: space-between;
-    }
-    p{
-      text-align: center;
-    }
-    .item{
-      max-width: 1024px;
-      margin: 0 auto;
-      height: 4rem;
-    }
-  }
+
+a {
+    font-size: 14px;
+    text-decoration: none;
+    color: #ffffff;
+}
+
+.footer {
+    padding: 40px 0;
+    background: #2651F6;
+}
+
+.line {
+    padding-bottom: 30px;
+    border-bottom: 1px solid #2b3136;
+}
+
+.container, .tool-container {
+    width: 1180px;
+    overflow: hidden;
+    margin: 0 auto;
+}
+
+.footer-navigation {
+    float: left;
+}
+
+.footer-tip {
+    float: right;
+    width: 270px;
+}
+
+.footer-navigation dl {
+    float: left;
+    margin: 0;
+    width: 130px;
+}
+
+dl {
+    display: block;
+    -webkit-margin-before: 1em;
+    -webkit-margin-after: 1em;
+    -webkit-margin-start: 0px;
+    -webkit-margin-end: 0px;
+}
+
+.footer-navigation dl.width-fix {
+    margin-right: 20px;
+    width: 145px;
+}
+
+.footer-navigation dl {
+    float: left;
+    margin: 0;
+    width: 130px;
+    color: #fff;
+}
+
+.footer-navigation dt, .footer-tip dt {
+    line-height: 24px;
+    font-size: 16px;
+}
+
+.footer-navigation dd {
+    margin: 12px 0 0;
+}
+
+.footer-navigation dd a .footer-tip dd {
+    color: #ffffff;
+}
+
+
+
+.footer-navigation dl.no-dt {
+    margin: 24px 0 0 40px;
+}
+
+.footer-tip {
+    float: right;
+    width: 270px;
+}
+
+.footer-tip dl {
+    margin: 0;
+}
+
+.qq dd {
+    margin-top: 8px;
+}
+
+.footer-tip dd {
+    margin: 0;
+    font-size: 14px;
+}
+
+.copyright {
+    display: block;
+    margin: 30px 0 0;
+    font-family: 'Helvetica Neue', 'Microsoft Yahei';
+    color: #fff;
+}
+
 </style>
